0f9a24fc6a6fffc83b9c03896e0801aab6842587
Eyleen <eyleenqh@hotmail.com>
2020-04-23 11:18:47 -0600

    Arreglo detalles

--- src/tareajavasockets/EchoMultiServer.java
+++ src/tareajavasockets/EchoMultiServer.java
@@ -74,6 +74,6 @@ public class EchoMultiServer {
     
      public static void main(String[] args) throws IOException {
         EchoMultiServer server=new EchoMultiServer();
-        server.start(6666);
+        server.start(5555);
     }
 }
